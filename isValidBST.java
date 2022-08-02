92787f97d4303298dc0cfec8c47d0992a06847c9
Saumya Srivastava <76783291+saumya-srivastava226@users.noreply.github.com>
2022-08-02 21:12:54 +0530

    Create isValidBST.java

---
+++ isValidBST.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    public boolean isValidBST(TreeNode root) 
+    
+    {
+        return isValidBSTHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        
+    }
+    public boolean isValidBSTHelper(TreeNode root, long minVal, long maxVal)
+    {
+        if(root==null)
+            return true;
+        if(root.val<=minVal||root.val>=maxVal)
+            return false;
+        return              isValidBSTHelper(root.left,minVal,root.val)&&isValidBSTHelper(root.right,root.val,maxVal);
+    }
+}
